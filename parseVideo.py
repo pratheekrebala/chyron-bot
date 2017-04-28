750984697afdcf3c997e8658bc97db1c02b5fd40
Pratheek Rebala <pratheekrebala@gmail.com>
2017-04-28 00:00:42 -0400

    Working Parser + OCR
    Signed-off-by: Pratheek Rebala <pratheekrebala@gmail.com>

--- parseVideo.py
+++ parseVideo.py
@@ -1,10 +1,13 @@
 import cv2
 import math
 import numpy as np
+import tesseract
+from PIL import Image
+import pytesseract
 
 #Load Video
 
-videoFile = "sample.mp4"
+videoFile = "test.mp4"
 vidcap = cv2.VideoCapture(videoFile)
 success,image = vidcap.read()
 
@@ -22,17 +25,22 @@ while success:
     success, image = vidcap.read()
     if frameId == 1:
         dimensions = dict(zip(('height', 'width', 'channels'), image.shape)) 
-        lowerThirdStart = dimensions['height'] - int(dimensions['height'] * 0.25)
+        lowerThirdStart = dimensions['height'] - int(dimensions['height'] * 0.21)
         lowerThirdEnd = dimensions['height'] - int(dimensions['height'] * 0.1)
-        lowerThirdRight = dimensions['width'] - int(dimensions['width'] * 0.14)
+        lowerThirdRight = dimensions['width'] - int(dimensions['width'] * 0.15)
+        lowerThirdLeft = int(dimensions['width'] * 0.05)
     if frameId % multiplier == 0:
-        lowerThird = image[lowerThirdStart:lowerThirdEnd,0:lowerThirdRight]
+        lowerThird = image[lowerThirdStart:lowerThirdEnd,lowerThirdLeft:lowerThirdRight]
         gray = cv2.cvtColor(lowerThird,cv2.COLOR_BGR2RGB)
         lower_white = np.array([200,200,200], dtype=np.uint8)
         upper_white = np.array([255,255,255], dtype=np.uint8)
         mask = cv2.inRange(lowerThird, lower_white, upper_white)
         res = cv2.bitwise_and(lowerThird,lowerThird,mask = mask)
-        cv2.imshow('frame', mask)
+        cv2.imshow('frame', lowerThird)
+        text = pytesseract.image_to_string(Image.fromarray(lowerThird))
+        if '\n' not in text:
+            print(text)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
